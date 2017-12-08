6dba17235e6f55c8ab95968cc604c9fffddd7046
gshein <sheingrisha@mail.ru>
2017-12-08 17:03:48 +0300

    some tasks

--- src/main/java/geekAlgorithms/homeTasks/Task1.java
+++ src/main/java/geekAlgorithms/homeTasks/Task1.java
@@ -41,17 +41,46 @@ public class Task1 {
 
     private void maxNumber () {
         String strData = "";
-        File file = new File("../resources/task1.txt");
+        int a = 0;
+        int b=0;
+        int c=0;
+        int d=0;
+        int mid1 = 0;
+        int mid2 = 0;
+        File file = new File("resources/task1.txt");
         try {
             System.out.println(file.getAbsolutePath());
             FileInputStream fistr = new FileInputStream(file);
             //BufferedInputStream bis = new BufferedInputStream(fistr);
             BufferedReader bis = new BufferedReader(new InputStreamReader(fistr));
-            while((strData = bis.readLine()) != null){
-                System.out.println(strData);
-            }
+            //while((strData = bis.readLine()) != null){}
+            strData = bis.readLine();
+            System.out.println(strData);
         } catch (IOException e) {
             System.out.println("Error! " + e.getStackTrace().toString());
+        }
+
+        a= Integer.parseInt(strData.split(" ")[0]);
+        b= Integer.parseInt(strData.split(" ")[1]);
+        c= Integer.parseInt(strData.split(" ")[2]);
+        d= Integer.parseInt(strData.split(" ")[3]);
+
+        if(a>b){
+            mid1 = a;
+        }else {
+            mid1 = b;
+        }
+
+        if(c>d){
+            mid2 = c;
+        }else {
+            mid2 = d;
+        }
+
+        if(mid1>mid2){
+            System.out.println("Max element is " + mid1);
+        }else {
+            System.out.println("Max element is " + mid2);
         }
 
     }
